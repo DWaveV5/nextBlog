ae9fa253865b8130cad5b784e6e940e9a32dd276
V5 <473212999@qq.com>
2024-04-22 02:03:59 +0800

    feat:支持切换暗黑模式

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ThemeButton";
 
 export default function page() {
   return (
     <>
       <div className="bg-red-400 mx-auto">hello nest.js</div>
+      <ModeToggle />
       <Button>Click me</Button>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Inter as FontSans } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -26,8 +27,10 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        外层的layout
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system">
+          外层的layout
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
