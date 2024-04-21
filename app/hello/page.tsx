06ac6cd42bfbe34084b4b248e07afee54e766fde
V5 <473212999@qq.com>
2024-04-22 01:55:25 +0800

    feat:引入shadcn/ui组件库

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 export default function page() {
   return (
-    <div className="bg-red-400 mx-auto">hello nest.js</div>
-  )
+    <>
+      <div className="bg-red-400 mx-auto">hello nest.js</div>
+      <Button>Click me</Button>
+    </>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "V5的Blog",
@@ -16,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="mx-auto">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         外层的layout
         {children}
       </body>
